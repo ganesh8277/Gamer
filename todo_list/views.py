7b88eeac6f43594c178c88b4a206cbee0a11a221
Ganesh <ganeshtarsd@gmail.com>
2020-05-04 17:48:23 +0530

    my first

---
+++ todo_list/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import TodoItem
+
+
+
+# Create your views here.
+def todoView(request):
+	all_todo_items=TodoItem.objects.all()
+	return render(request,'todo.html',
+		{'all_items':all_todo_items})
+		
+         
